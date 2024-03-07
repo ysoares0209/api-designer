65d9768b363d09bd437e8760de69a27bb8bf094a
ysoares0209 <yan@westpoint.io>
2024-03-06 22:34:40 -0300

    feat: skeleton implementation of the sign up page

--- web-app/src/components/pages/SignIn.vue
+++ web-app/src/components/pages/SignIn.vue
@@ -1,7 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Title from '../atoms/Title.vue';
+import Typography from '../atoms/Typography.vue';
+import Link from '../atoms/Link.vue';
+</script>
 
 <template>
-  <h1>Sign in page</h1>
+  <main class="page-container">
+    <header>
+      <span>Logo placeholder</span>
+      <p>Already have an account? <a>Sign In</a></p>
+    </header>
+    <section>
+      <h1>Sign Up</h1>
+      <div class="input">
+        <label for="email">Email</label>
+        <input id="email" />
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input id="password" />
+      </div>
+      <div class="input">
+        <label for="confirmPassword">Confirm password</label>
+        <input id="confirmPassword" />
+      </div>
+      <button>Sign up</button>
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page-container {
+  max-width: 1480px;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 4rem 2rem;
+  font-weight: normal;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+}
+
+section {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+}
+</style>
