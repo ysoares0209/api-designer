544409abde167fea6670da7653d46bd4a1922f0a
ysoares0209 <yan@westpoint.io>
2024-03-09 16:24:20 -0300

    fix: fix Link atom incorrect handle of external links

--- web-app/src/components/molecules/TextWithLink.vue
+++ web-app/src/components/molecules/TextWithLink.vue
@@ -2,13 +2,13 @@
   import Typography from '../atoms/Typography.vue';
   import Link from '../atoms/Link.vue';
 
-  defineProps(['text', 'linkText', 'linkUrl']);
+  defineProps(['text', 'linkText', 'linkUrl', 'isExternalLink']);
 </script>
 
 <template>
   <div class="TextWithLinkWrapper">
     <Typography :text="text" />
-    <Link :link="linkUrl" :text="linkText" />
+    <Link :link="linkUrl" :text="linkText" :isExternal="isExternalLink" />
   </div>
 </template>
 
