c18390e40bee896b111c1623f37bdd62a7a2befc
ysoares0209 <yan@westpoint.io>
2024-03-09 18:40:19 -0300

    feat: base implementation for CodeInput

---
+++ web-app/src/components/atoms/CodeInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <input type="number" maxlength="1" min="0" max="9" />
+</template>
+
+<style scoped>
+  input {
+    height: 3rem;
+    width: 3rem;
+    border: 1px solid #9095A1;
+    border-radius: 6px;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: #171A1F;
+    text-align: center;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
+    appearance: textfield;
+  }
+</style>
