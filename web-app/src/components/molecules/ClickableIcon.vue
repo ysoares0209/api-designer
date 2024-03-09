aa29dbf3bc55aa9ae588932e823c7b49cca569c2
ysoares0209 <yan@westpoint.io>
2024-03-09 16:18:12 -0300

    feat: add ClickableIcon molecule

---
+++ web-app/src/components/molecules/ClickableIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps(['onClick'])
+</script>
+
+<template>
+  <button @click="onClick">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+ button {
+  padding: 0;
+  margin: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+ }
+</style>
