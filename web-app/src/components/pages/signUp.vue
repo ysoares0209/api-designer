98a100298c0add21d49469c390a9e191bec289a7
ysoares0209 <yan@westpoint.io>
2024-03-09 12:25:16 -0300

    feat[Atomic Design]: break down logic further by introducing AuthPages template

--- web-app/src/components/pages/signUp.vue
+++ web-app/src/components/pages/signUp.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-import AuthPagesHeader from '../organisms/AuthPagesHeader.vue';
+import AuthPagesTemplate from '../Templates/AuthPages.vue';
 import AuthPagesSignUpForm from '../organisms/AuthPagesSignUpForm.vue';
 </script>
 
 <template>
-  <main class="page-container">
-    <AuthPagesHeader text="Already have an account?" linkText="Sign In" linkUrl="/" />
+  <AuthPagesTemplate text="Already have an account?" linkText="Sign In" linkUrl="/sign-in">
     <AuthPagesSignUpForm />
-  </main>
+  </AuthPagesTemplate>
 </template>
 
 <style scoped>
-.page-container {
-  max-width: 1480px;
-  height: 100vh;
-  margin: 0 auto;
-  padding: 4rem 2rem;
-  font-weight: normal;
-}
-
 </style>
