7ff34aaec474017620d605c0a9eb145629d1a1a4
ysoares0209 <yan@westpoint.io>
2024-03-09 16:18:45 -0300

    feat: Update Modal molecule to only have core Modal logic

--- web-app/src/components/molecules/Modal.vue
+++ web-app/src/components/molecules/Modal.vue
@@ -1,37 +1,20 @@
 <script setup lang="ts">
 import Overlay from '../atoms/Overlay.vue';
-import xIcon from '../../assets/Icons/XIcon.vue';
-import Title from '../atoms/Title.vue';
-import Typography from '../atoms/Typography.vue';
 </script>
 
 <template>
   <Overlay />
   <div class="ModalWrapper">
-    <div class="ConfirmEmailModal">
-      <div class="ModalHeader">
-        <xIcon />
-        <Title text="Almost done" />
-      </div>
-      <Typography text="Please type the code we sent you in your email" />
-      <div class="CodeInputWrapper">
-        <input />
-        <input />
-        <input />
-        <input />
-        <input />
-        <input />
-      </div>
-      <button>Verify</button>
-      <p>Can't access to your email? <a href="mailto:yansoaressilva@outlook.com">Get In contact</a></p>
-    </div>
+    <slot></slot>
   </div>
 </template>
 
-<style>
+<style scoped>
   .ModalWrapper {
     width: 35%;
     height: 45%;
+    min-width: 35rem;
+    min-height: 28rem;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -39,45 +22,5 @@ import Typography from '../atoms/Typography.vue';
     background-color: #FFFFFF; /* Black background with opacity */
     z-index: 2;
     border-radius: 12px;
-  }
-
-  .ConfirmEmailModal {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    padding: 4rem 4rem 2rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 2rem;
-  }
-
-  .ModalHeader {
-    svg {
-      position: absolute;
-      right: 3rem;
-      top: 2rem;
-    }
-  }
-
-  .CodeInputWrapper {
-    display: flex;
-    gap: 1rem;
-
-    input {
-      height: 3rem;
-      width: 3rem;
-      border-color: #9095A1;
-      border-radius: 6px;
-      font-size: 1.5rem;
-      font-weight: 700;
-      color: #171A1F;
-      text-align: center;
-    }
-
-    input:focus {
-      outline: none;
-    }
   }
 </style>
