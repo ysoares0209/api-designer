13d7f614cb1e74806d018e16d55c745f66315906
ysoares0209 <yan@westpoint.io>
2024-03-11 19:44:00 -0300

    chore: setup prettier (w/ lint)

--- infrastructure/.eslintrc.js
+++ infrastructure/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "standard-with-typescript",
+  extends: ["standard-with-typescript", "plugin:prettier/recommended"],
   overrides: [
     {
       env: {
