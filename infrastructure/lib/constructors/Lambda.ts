b428bc3c1f7619e3e038f7fe1e796313398fa3de
ysoares0209 <yan@westpoint.io>
2024-03-12 18:38:50 -0300

    feat: add envs to be passed as props to lambda constructor

--- infrastructure/lib/constructors/Lambda.ts
+++ infrastructure/lib/constructors/Lambda.ts
@@ -8,6 +8,7 @@ interface Props {
   name: string;
   description: string;
   entry: string;
+  envs: Record<string, string>;
   duration?: number;
 }
 
@@ -17,12 +18,13 @@ export class Lambda extends Construct {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id);
 
-    const { name, description, entry, duration } = props;
+    const { name, description, entry, envs, duration } = props;
     this.function = new NodejsFunction(this, "Function", {
       functionName: name,
       description,
       entry,
       handler: "handler",
+      environment: envs,
       runtime: Runtime.NODEJS_20_X,
       logRetention: RetentionDays.ONE_WEEK,
       timeout: Duration.seconds(duration || 3),
