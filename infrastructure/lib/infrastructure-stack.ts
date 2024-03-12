9bcfcfeabb137b28d60160b6e0f64cbd4a24f169
ysoares0209 <yan@westpoint.io>
2024-03-12 18:39:31 -0300

    feat: configure stack to link sign up lambda with api endpoint

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,7 @@
-import { Stack, StackProps } from "aws-cdk-lib";
 import type { Construct } from "constructs";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import { HttpMethod } from "aws-cdk-lib/aws-apigatewayv2";
 
 import { HttpApi } from "./constructors/HttpApi";
 import { Lambda } from "./constructors/Lambda";
@@ -18,8 +20,25 @@ export class InfrastructureStack extends Stack {
     const { function: SignUpLambda } = new Lambda(this, "SignUpLambda", {
       name: "signUpLambda",
       description: "Lambda used to sign up an user",
-      entry: "src/handlers/signUpLambda.ts",
+      entry: "src/handlers/SignUpLambda.ts",
+      envs: {
+        TABLE_NAME: table.tableName,
+        COGNITO_USER_POOL_ID: userPool.userPoolId,
+        COGNITO_CLIENT_ID: client.userPoolClientId,
+      },
       duration: 5,
+    });
+    table.grantWriteData(SignUpLambda);
+
+    // endpoints
+    const SignUpLambdaIntegration = new HttpLambdaIntegration(
+      "SignUpLambdaIntegration",
+      SignUpLambda,
+    );
+    api.addRoutes({
+      path: "/sign-up",
+      methods: [HttpMethod.POST],
+      integration: SignUpLambdaIntegration,
     });
   }
 }
