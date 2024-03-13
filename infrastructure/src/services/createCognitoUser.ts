79d595567012622852a23e32db39fe710c53d4ee
ysoares0209 <yan@westpoint.io>
2024-03-12 21:22:29 -0300

    feat: add createCognitoUser helper

---
+++ infrastructure/src/services/createCognitoUser.ts
@@ -0,0 +1,18 @@
+import { SignUpCommand, SignUpCommandInput } from "@aws-sdk/client-cognito-identity-provider";
+import client from "../clients/cognito";
+
+export default async function createCognitoUser(email: string, password: string) {
+  const clientId = process.env.COGNITO_CLIENT_ID;
+  if (!clientId) throw new Error("Client id not found!");
+
+  const signUpParams: SignUpCommandInput = {
+    ClientId: clientId,
+    Username: email,
+    Password: password,
+  };
+
+  const signUpCommand = new SignUpCommand(signUpParams);
+  const response = await client.send(signUpCommand);
+
+  return { success: response.UserConfirmed };
+}
