32170f0b773cf0f6bdc674c013c2814e037dfd87
ysoares0209 <yan@westpoint.io>
2024-03-14 19:17:38 -0300

    feat: add parseBody helper function

---
+++ infrastructure/src/helpers/parseBody.ts
@@ -0,0 +1,9 @@
+/* 
+  decided to use any here because that's essentially what body can be
+
+*/
+export default function parseBody(body: string | null | Record<string, unknown>): any {
+  if (!body) return {};
+  if (typeof body === "string") return JSON.stringify(body);
+  return body;
+}
