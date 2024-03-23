0d5b53aee6309a4af1fe327148a19f5d161cfa5a
ysoares0209 <yan@westpoint.io>
2024-03-23 17:17:47 -0300

    feat: add & setup oh-vue-icons package

--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -1,10 +1,15 @@
 import './assets/base.css';
 
 import { createApp } from 'vue';
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { BiExclamationTriangleFill, BiCheckCircleFill, BiXLg } from 'oh-vue-icons/icons';
 import App from './App.vue';
 import router from './router.ts';
 
+addIcons(BiExclamationTriangleFill, BiCheckCircleFill, BiXLg);
+
 const app = createApp(App);
 
+app.component('v-icon', OhVueIcon);
 app.use(router);
 app.mount('#app');
