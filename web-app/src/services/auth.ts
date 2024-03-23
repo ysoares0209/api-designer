39bd81030b8edb4200c4b2c82ffec5ad7a2b059f
ysoares0209 <yan@westpoint.io>
2024-03-23 12:42:26 -0300

    feat: add auth service

---
+++ web-app/src/services/auth.ts
@@ -0,0 +1,15 @@
+const baseApiUrl = (import.meta as any).env.VITE_API_URL;
+
+interface SignUpUserProps {
+  email: string;
+  password: string;
+}
+export const signUpUser = async ({ email, password }: SignUpUserProps) => {
+  const result = await fetch(`${baseApiUrl}/auth/sign-up`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ userEmail: email, userPassword: password })
+  });
+  const data = await result.json();
+  return data;
+};
