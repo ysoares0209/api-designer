d54671b04b5f91596cfcaa33c018f54f5c1289cd
ysoares0209 <yan@westpoint.io>
2024-03-24 19:37:54 -0300

    feat: add NotificationsSnackbar to App.vue

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router';
+  import NotificationsSnackbar from './components/organisms/NotificationsSnackbar.vue';
 </script>
 
 <template>
   <RouterView />
+  <NotificationsSnackbar />
 </template>
