7c507f71068ecf2f54e1062972f6ab8eaf83710d
ysoares0209 <yan@westpoint.io>
2024-03-24 18:37:14 -0300

    feat: setup pinia

--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -1,6 +1,7 @@
 import './assets/base.css';
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import { OhVueIcon, addIcons } from 'oh-vue-icons';
 import { BiExclamationTriangleFill, BiCheckCircleFill, BiXLg } from 'oh-vue-icons/icons';
 import App from './App.vue';
@@ -8,8 +9,11 @@ import router from './router.ts';
 
 addIcons(BiExclamationTriangleFill, BiCheckCircleFill, BiXLg);
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.component('v-icon', OhVueIcon);
+
 app.use(router);
+app.use(pinia);
 app.mount('#app');
