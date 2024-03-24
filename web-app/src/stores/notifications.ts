4e6a8e5db5162bb394c67c747a24daaaeb2c1e49
ysoares0209 <yan@westpoint.io>
2024-03-24 19:04:44 -0300

    feat: add notificationColor & iconName to be automatically computed by notification store

--- web-app/src/stores/notifications.ts
+++ web-app/src/stores/notifications.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
 type NotificationType = 'success' | 'error';
@@ -12,8 +12,20 @@ const initialState = {
 const durationOffset = 300;
 
 export const useNotificationsStore = defineStore('notifications', () => {
+  // state
   const state = ref(initialState);
 
+  // getters
+  const notificationColor = computed(() => {
+    if (state.value.type === 'success') return '#63b365';
+    return '#f22e2e';
+  });
+  const iconName = computed(() => {
+    if (state.value.type === 'success') return 'bi-check-circle-fill';
+    return 'bi-exclamation-triangle-fill';
+  });
+
+  // methods
   function showNotification(message: string, type: NotificationType, duration: number = 2500) {
     state.value = {
       isVisible: true,
@@ -28,6 +40,8 @@ export const useNotificationsStore = defineStore('notifications', () => {
 
   return {
     state,
+    notificationColor,
+    iconName,
     showNotification
   };
 });
