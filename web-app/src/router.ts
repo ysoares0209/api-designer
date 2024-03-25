b51cddccd17ba65eed84093ddf69cbb4e4635666
ysoares0209 <yan@westpoint.io>
2024-03-24 23:28:35 -0300

    feat: add base skeleton for sign in page

--- web-app/src/router.ts
+++ web-app/src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './components/pages/Home.vue';
 import SignUpPage from './components/pages/signUp.vue';
+import SignInPage from './components/pages/SignIn.vue';
 
 const routes = [
   { path: '/', name: 'home', component: HomePage },
-  { path: '/sign-up', name: 'Sign up', component: SignUpPage }
+  { path: '/sign-up', name: 'Sign up', component: SignUpPage },
+  { path: '/sign-in', name: 'Sign in', component: SignInPage }
 ];
 
 const router = createRouter({
